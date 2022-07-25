9666aa32c6a22743a8cb2537980762d6fa1ebb81
Jairo Matos da Rocha <devjairorm@gmail.com>
2022-07-25 00:58:46 -0300

    blue and isort

--- update_covid19/db.py
+++ update_covid19/db.py
@@ -1,27 +1,26 @@
-
-from sqlalchemy import create_engine
 import psycopg2
-
+from sqlalchemy import create_engine
 
 engine = create_engine(
-        'postgresql+psycopg2://postgres:password@localhost/postgres',
-        )
-         
-def save_db(data_frame,table_name,model):
-    postgreSQLConnection  = engine.connect()
+    'postgresql+psycopg2://postgres:password@localhost/postgres',
+)
+
+
+def save_db(data_frame, table_name, model):
+    postgreSQLConnection = engine.connect()
     try:
         data_frame.to_sql(
-            table_name,
-            postgreSQLConnection, 
-            if_exists='append',
-            dtype=model
-            )
-            
+            table_name, postgreSQLConnection, if_exists='append', dtype=model
+        )
+
     except ValueError as vx:
         print(vx)
-    except Exception as ex:  
+    except Exception as ex:
         print(ex)
     else:
-        print("PostgreSQL Table %s has been created successfully."%postgreSQLTable);
+        print(
+            'PostgreSQL Table %s has been created successfully.'
+            % postgreSQLTable
+        )
     finally:
         postgreSQLConnection.close()
--- update_covid19/models/covid19.py
+++ update_covid19/models/covid19.py
@@ -1,9 +1,12 @@
 from email.policy import default
-from sqlalchemy import Integer, String, DateTime, Column
+
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 from update_covid19.db import engine
 
 Base = declarative_base()
+
 
 class Casos(Base):
     __tablename__ = 'casos'
@@ -52,84 +55,83 @@ class Obitos(Base):
     municipio = Column(String(60))
 
 
-Base.metadata.create_all(engine) 
+Base.metadata.create_all(engine)
 
 constructor = {
-    'queryConfimdos':{
-        'obitos':None,
-        'confirmados':None,
-        },
-    'feminino':{
-        'coll':'sexo',
-        'where':'FEMININO',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'masculino':{
-        'coll':'sexo',
-        'where':'MASCULINO',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'menor10':{
-        'coll':'faixa_etaria',
-        'where':'< 10 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de10a14':{
-        'coll':'faixa_etaria',
-        'where':'10 a 14 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de15a19':{
-        'coll':'faixa_etaria',
-        'where':'15 a 19 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de20a29':{
-        'coll':'faixa_etaria',
-        'where':'20 a 29 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de30a39':{
-        'coll':'faixa_etaria',
-        'where':'30 a 39 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de40a49':{
-        'coll':'faixa_etaria',
-        'where':'40 a 49 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de50a59':{
-        'coll':'faixa_etaria',
-        'where':'50 a 59 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de60a69':{
-        'coll':'faixa_etaria',
-        'where':'60 a 69 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'de70a79':{
-        'coll':'faixa_etaria',
-        'where':'70 a 79  anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    'maior80':{
-        'coll':'faixa_etaria',
-        'where':'>= 80 anos',
-        'obitos':None,
-        'confirmados':None,
-        },
-    
+    'queryConfimdos': {
+        'obitos': None,
+        'confirmados': None,
+    },
+    'feminino': {
+        'coll': 'sexo',
+        'where': 'FEMININO',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'masculino': {
+        'coll': 'sexo',
+        'where': 'MASCULINO',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'menor10': {
+        'coll': 'faixa_etaria',
+        'where': '< 10 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de10a14': {
+        'coll': 'faixa_etaria',
+        'where': '10 a 14 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de15a19': {
+        'coll': 'faixa_etaria',
+        'where': '15 a 19 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de20a29': {
+        'coll': 'faixa_etaria',
+        'where': '20 a 29 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de30a39': {
+        'coll': 'faixa_etaria',
+        'where': '30 a 39 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de40a49': {
+        'coll': 'faixa_etaria',
+        'where': '40 a 49 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de50a59': {
+        'coll': 'faixa_etaria',
+        'where': '50 a 59 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de60a69': {
+        'coll': 'faixa_etaria',
+        'where': '60 a 69 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'de70a79': {
+        'coll': 'faixa_etaria',
+        'where': '70 a 79  anos',
+        'obitos': None,
+        'confirmados': None,
+    },
+    'maior80': {
+        'coll': 'faixa_etaria',
+        'where': '>= 80 anos',
+        'obitos': None,
+        'confirmados': None,
+    },
 }
